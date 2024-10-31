1db824bedb9b3ad5559deb57db97efb10419179f
Jozef Sitarcik <jozefsitarcik@gmail.com>
2024-10-31 10:38:42 +0100

    removed datasets

--- isv/src/core.py
+++ isv/src/core.py
@@ -4,7 +4,6 @@ SRC_DIR = os.path.abspath(os.path.dirname(__file__))
 
 PACKAGE_DIR = os.path.dirname(SRC_DIR)
 MODELS_DIR = os.path.join(PACKAGE_DIR, "models")
-TRAIN_DATASET_DIR = os.path.join(PACKAGE_DIR, "datasets")
 
 HAPLOINSUFFICIENCY_SCORES = [1, 2, 3]
 TRIPLOSENSITIVITY_SCORES = [1, 2, 3]
